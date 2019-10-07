432898cf1b6228865bb16597f6ba1dbf4ce350d7
FUJITA Setsu <fjt.seth@gmail.com>
2019-10-07 10:31:59 +0900

    build: bump library version

--- packages/yomo-yama-frontend/gatsby-config.js
+++ packages/yomo-yama-frontend/gatsby-config.js
@@ -27,8 +27,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `yomoyama-no-yama`,
+        short_name: `yomo`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
